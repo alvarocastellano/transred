a75bbd5aea03539f5856822d6209a3e174eb48f0
alvarocastellano <alvcasalo@alum.us.es>
2024-02-24 18:44:34 +0100

    add: v1.1 transred

--- js/script.js
+++ js/script.js
@@ -1,18 +1,22 @@
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function () {
+    // --- First carousel logic ---
+  
     let currentIndex = 0;
     const totalSlides = document.querySelectorAll(".slide").length;
-
+  
     function showSlide(index) {
-        const slider = document.querySelector(".slider");
-        slider.style.transform = `translateX(${-index * 100}%)`;
+      const slider = document.querySelector(".slider");
+      slider.style.transform = `translateX(${-index * 100}%)`;
     }
-
+  
     function nextSlide() {
-        currentIndex = (currentIndex + 1) % totalSlides;
-        showSlide(currentIndex);
+      currentIndex = (currentIndex + 1) % totalSlides;
+      showSlide(currentIndex);
     }
-
-    setInterval(nextSlide, 5000);
-
-    showSlide(currentIndex);
-});
+  
+    setInterval(nextSlide, 2000);
+    showSlide(currentIndex); // Show initial slide
+  
+    // --- Second carousel to do
+  });
+  
